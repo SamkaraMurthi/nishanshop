5f86d6c492105b5f075d4ec4a137d41cf895d61e
Ichi Pravda <ichipravda@IchiPravda.IchiPravda.com>
2024-01-05 01:01:05 +0100

    chart.html modification

---
+++ js/chart.js
@@ -0,0 +1,42 @@
+let emailBody = "";
+document.addEventListener('DOMContentLoaded', function () {
+    // Retrieve values from local storage
+    var selectedIndexTarrot = localStorage.getItem('tarrot-select');
+    var selectedIndexConst = localStorage.getItem('const-select');
+
+    // Display the selected values in the appropriate elements
+    var tarrotDisplay = document.getElementById('tarrot-display');
+    tarrotDisplay.textContent = selectedIndexTarrot;
+
+    var constDisplay = document.getElementById('const-display');
+    constDisplay.textContent = selectedIndexConst;
+
+    // Calculate and display total values
+    var tarrotTotal = selectedIndexTarrot * 30;
+    var constTotal = selectedIndexConst * 20;
+    let totalOrder = tarrotTotal + constTotal;
+
+    var totalTarotElement = document.getElementById('total-tarrot');
+    totalTarotElement.textContent = tarrotTotal + "PLN";
+
+    var constTotalElement = document.getElementById('total-const');
+    constTotalElement.textContent = constTotal + "PLN";
+
+    let totalCost = document.getElementById('total-cost');
+    totalCost.textContent = "Your Total is : " + totalOrder + "PLN"
+
+    let emailBody = "Hello You have order of Tarrot reading " + selectedIndexTarrot + " Session and Consulation " + selectedIndexConst + " Session";
+
+    sendEmail(emailBody);
+    
+});
+
+function sendEmail(){
+    const to = 'recipient@example.com';
+      const subject = 'Reservation Order';
+      const content = emailBody;
+
+      const mailtoLink = `mailto:${to}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(content)}`;
+
+      window.location.href = mailtoLink;
+    }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-let tarrotPurchase = [1,2,3,4,5];
-let consulPurchase = [1,2,3,4,5];
+function Reserve() {
+    let tarrotChoose = document.getElementById('tarrot-select').value;
+    let constChoose = document.getElementById('const-select').value;
+    let intTarrotChoose = parseInt(tarrotChoose, 10);
+    let intConstChoose = parseInt(constChoose, 10);
+    let tarrotTotal = intTarrotChoose * 30;
+    let constTotal = intConstChoose * 20;
 
+    if (intTarrotChoose === 0 && intConstChoose === 0) {
+        alert("You can't order 0 items for both Tarot and Constellation");
+    } else {
+        // Store the values in local storage
+        localStorage.setItem('tarrot-select', intTarrotChoose);
+        localStorage.setItem('const-select', intConstChoose);
+
+        // Redirect to chart.html
+        window.location.href = 'chart.html';
+    }
+}
